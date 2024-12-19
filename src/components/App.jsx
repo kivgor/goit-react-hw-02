86b661d36bf5dd1d558e9d4f8b829e0a0d3472fe
Igor <pa220282kiv@gmail.com>
2024-12-19 17:22:12 +0200

    Step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,15 @@
 // import reactLogo from '../assets/react.svg'
 // import viteLogo from '/vite.svg'
 
+
 import Description from './Description/Description';
 import Feedback from './Feedback/Feedback';
 import Options from './Options/Options';
 
-function App() {
+function App() {  
   
-  // const count = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
 
+   
   // const [count, setCount] = useState(0);
 
   return (
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,22 +1,54 @@
 import css from './Options.module.css';
+import { useState } from 'react';
 
 const Options = () => {
+
+const [countVote, setCountVote] = useState({
+  good: 0,
+  neutral: 0,
+  bad: 0,
+});
+  
+   const handleClickByVote = vote => {
+     switch (vote) {
+       case 'good':
+         return setCountVote(prev => ({ ...prev, good: prev.good + 1 }));
+       case 'neutral':
+         return setCountVote(prev => ({ ...prev, neutral: prev.neutral + 1 }));
+       case 'bad':
+         return setCountVote(prev => ({ ...prev, bad: prev.bad + 1 }));
+       default:
+         break;
+     }
+   };
 
   return (
     <ul className={css.list}>
       <li>
-        <button className={css.button} type="button">
-          Good
+        <button
+          onClick={() => handleClickByVote('good')}
+          className={css.button}
+          type="button"
+        >
+          Good: {countVote.good}
         </button>
       </li>
       <li>
-        <button className={css.button} type="button">
-          Neutral
+        <button
+          onClick={() => handleClickByVote('neutral')}
+          className={css.button}
+          type="button"
+        >
+          Neutral: {countVote.neutral}
         </button>
       </li>
       <li>
-        <button className={css.button} type="button">
-          Bad
+        <button
+          onClick={() => handleClickByVote('bad')}
+          className={css.button}
+          type="button"
+        >
+          Bad: {countVote.bad}
         </button>
       </li>
       <li>
